c9fb7870a8e34053f36b37f2eabf8e9105fe8f23
Brooks Benson <brooksbenson03@gmail.com>
2020-10-19 09:23:03 -0600

    updates

---
+++ expiration/src/index.ts
@@ -0,0 +1,35 @@
+import { natsWrapper } from './nats-wrapper';
+import { OrderCreatedListener } from './events/listeners/order-created-listener';
+
+const start = async () => {
+  const { NATS_CLIENT_ID, NATS_URL, NATS_CLUSTER_ID } = process.env
+
+  if (!NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID must be defined')
+  }
+
+  if (!NATS_URL) {
+    throw new Error('NATS_URL must be defined')
+  }
+
+  if (!NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID must be defined')
+  }
+
+  try {
+    // connect to NATS
+    await natsWrapper.connect(NATS_CLUSTER_ID, NATS_CLIENT_ID, NATS_URL)
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed')
+      process.exit()
+    })
+    process.on('SIGINT', () => natsWrapper.client.close())
+    process.on('SIGTERM', () => natsWrapper.client.close())
+
+    new OrderCreatedListener(natsWrapper.client).listen()
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+start()
